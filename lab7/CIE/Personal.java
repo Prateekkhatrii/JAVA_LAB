95a45aa683e7203302dc4c578acfbd2ebd9400e4
Prateekkhatrii <prateekkhatri028@gmail.com>
2025-12-17 20:22:22 +0530

    Create Personal class with attributes and constructor

---
+++ lab7/CIE/Personal.java
@@ -0,0 +1,11 @@
+package CIE;
+public class Personal {
+    public String usn, name;
+    public int sem;
+
+    public Student(String usn, String name, int sem){
+        this.usn = usn;
+        this.name = name;
+        this.sem = sem;
+    }
+}
