0c7876fffb0568cf37a42c00dc281b46662a9a91
Prateekkhatrii <prateekkhatri028@gmail.com>
2025-12-17 21:15:34 +0530

    Add External.java file

--- lab7/SEE/External.java
+++ lab7/SEE/External.java
@@ -1,10 +1,13 @@
 package SEE;
+
 import CIE.Personal;
+
 public class External extends Personal {
     public int[] externalMarks = new int[5];
-    public External(String usn, String name, int sem, int[] ext){
+
+    public External(String usn, String name, int sem, int[] ext) {
         super(usn, name, sem);
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             externalMarks[i] = ext[i];
         }
     }
