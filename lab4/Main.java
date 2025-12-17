ed44f0ddf6d050c7050de27bbb2a2cccda177781
Prateekkhatrii <prateekkhatri028@gmail.com>
2025-12-17 20:11:39 +0530

    Implement shape classes with area calculation

---
+++ lab4/Main.java
@@ -0,0 +1,48 @@
+abstract class Shape {
+    int a, b;
+    abstract void printArea();
+}
+
+class Rectangle extends Shape {
+    Rectangle(int length, int breadth) {
+        a = length;
+        b = breadth;
+    }
+
+    void printArea() {
+        System.out.println("Area of Rectangle = " + (a * b));
+    }
+}
+
+class Triangle extends Shape {
+    Triangle(int base, int height) {
+        a = base;
+        b = height;
+    }
+
+    void printArea() {
+        System.out.println("Area of Triangle = " + (0.5 * a * b));
+    }
+}
+
+class Circle extends Shape {
+    Circle(int radius) {
+        a = radius;
+    }
+
+    void printArea() {
+        System.out.println("Area of Circle = " + (3.14 * a * a));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(10, 5);
+        Shape s2 = new Triangle(6, 4);
+        Shape s3 = new Circle(7);
+
+        s1.printArea();
+        s2.printArea();
+        s3.printArea();
+    }
+}
