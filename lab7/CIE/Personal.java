31973634d9442cf22d473af162a960c12ea5f880
Prateekkhatrii <prateekkhatri028@gmail.com>
2025-12-17 21:15:00 +0530

    Update Personal.java

--- lab7/CIE/Personal.java
+++ lab7/CIE/Personal.java
@@ -1,9 +1,10 @@
 package CIE;
+
 public class Personal {
     public String usn, name;
     public int sem;
 
-    public Student(String usn, String name, int sem){
+    public Personal(String usn, String name, int sem) {
         this.usn = usn;
         this.name = name;
         this.sem = sem;
